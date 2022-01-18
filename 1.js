1bd2a5a3d990197f835e56413ed4a08251a1fd97
any86 <383514580@qq.com>
2022-01-18 21:03:50 +0800

    feat: 增加isSameScheme, 判断2个数据结构是否相同

--- 1.js
+++ 1.js
@@ -1,3 +1,6 @@
-const getType = require('./dist/getType').default
-const a = getType('[1,2]');
-console.log(a);
+const getType = require('./out/getType').default
+const { isSameScheme } = require('./out/getType')
+const t = getType(`{a:100,b:[10,'ww'],c:'100'}`);
+// const t = getType(`[1,'222,',[[133], '123',], { a: [123,{aaa:['www',1]}] }]`);
+console.log(t);
+// console.log(isSameScheme([1,{ c: 22,d:[null] },3], [2,{c: 1 ,d:[null]},1]));
--- src/getType.ts
+++ src/getType.ts
@@ -1,4 +1,4 @@
-import JSON5 from 'json5'
+import JSON5 from 'json5';
 import prettier from 'prettier';
 type KV = Record<string | number | symbol, unknown>;
 /**
@@ -8,120 +8,168 @@ type KV = Record<string | number | symbol, unknown>;
  * @returns typescript类型
  */
 export default function (jsonString: string, space = '    ') {
+    // const is = isAllSameItemArray([[1], [2]])
+    // console.warn(is);
 
-  const is = isAllSameItemArray([[1], [2]])
-  console.warn(is);
+    const json = JSON5.parse(jsonString);
+    const typeArray: string[] = [`type ResponseData=`];
+    walk(json);
+    /**
+     * 遍历节点
+     * @param node 节点
+     * @param propsName 节点名称
+     * @param level 当前层级, 用来控制缩进
+     */
+    function walk(node: unknown, propsName?: string, parentType?: string, level = 0) {
+        const indent = space.repeat(level);
+        // 对象
+        if (isObject(node)) {
+            if (void 0 === propsName) {
+                typeArray.push(`${indent}{`);
+            } else {
+                typeArray.push(`${indent}${propsName}:{`);
+            }
 
-
-
-  const json = JSON5.parse(jsonString);
-  const typeArray: string[] = [`type ResponseData=`];
-  walk(json);
-  /**
-   * 遍历节点
-   * @param node 节点
-   * @param propsName 节点名称 
-   * @param level 当前层级, 用来控制缩进
-   */
-  function walk(node: unknown, propsName?: string, level = 0) {
-    const indent = space.repeat(level);
-    if (isObject(node)) {
-      if (void 0 === propsName) {
-        typeArray.push(`${indent}{`);
-      } else {
-        typeArray.push(`${indent}${propsName}:{`);
-      }
-      for (let key in node) {
-        walk(node[key], key, level + 1);
-      }
-      typeArray.push(`${indent}};`);
-    } else if (isArray(node)) {
-      // 顶级是个数组
-      if (0 === level) {
-        let currentType = '';
-        for (let childNode of node) {
-          currentType = typeof childNode
-          walk(childNode, void 0, level + 1);
+            for (let key in node) {
+                walk(node[key], key, 'object', level + 1);
+            }
+            typeArray.push(`${indent}}` + (void 0 === propsName ? '' : ';'));
         }
-      } else {
-        walk(node[0], propsName, level);
-        const last = typeArray.pop();
-        if (last) {
-          typeArray.push(last.replace(';', '[];'));
+        // 数组
+        else if (isArray(node)) {
+            // true就是数组
+            // false就是元组
+            let isAllSameChildNode = true;
+            let prevNode;
+            for (let currentNode of node) {
+                // console.log({prevNode, currentNode,node});
+
+                if (void 0 !== prevNode) {
+                    isAllSameChildNode = isSameScheme(prevNode, currentNode);
+                    if (!isAllSameChildNode) {
+                        break;
+                    }
+                }
+                prevNode = currentNode;
+            }
+            // 当前是顶级,数组
+            if (isAllSameChildNode) {
+                // typeArray.push(void 0 !== propsName ? `${propsName}:` : '');
+                walk(node[0], propsName, 'array', level + 1);
+                typeArray.push('[]');
+            }
+            // 元组
+            else {
+                typeArray.push(void 0 !== propsName ? `${propsName}:[` : '[');
+                for (let childNode of node) {
+                    walk(childNode, void 0, 'array', level + 1);
+                    typeArray.push(`,`);
+                }
+                // 删除尾部","
+                typeArray.pop();
+                typeArray.push(`]`);
+            }
+            if(propsName) typeArray.push(';');
+
+
         }
-      }
-
-
-
-    } else {
-      if (void 0 === propsName) {
-        typeArray.push(`${indent}${typeof node}`);
-      } else {
-        typeArray.push(`${indent}${propsName}:${typeof node};`);
-      }
+        // 简单类型
+        else {
+            const type = typeof node;
+            if (void 0 === propsName) {
+                typeArray.push(`${indent}${type}`);
+            } else {
+                typeArray.push(`${indent}${propsName}:${type}` + ('array' === parentType ? '' : ';'));
+            }
+        }
     }
-  }
 
-  return ((typeArray.join('\r\n')));
+    const typeString = typeArray.join('');
+    // return typeString;
+
+    return prettier.format(typeString, {parser:'typescript'});
 }
 
 
 function isObject(node: unknown): node is KV {
-  return '[object Object]' === Object.prototype.toString.call(node);
+    return '[object Object]' === Object.prototype.toString.call(node);
 }
 
 function isArray(node: unknown): node is unknown[] {
-  return '[object Array]' === Object.prototype.toString.call(node);
+    return '[object Array]' === Object.prototype.toString.call(node);
 }
 
+// function hasSameKeysObject(obj1: KV, obj2: KV): boolean {
+//     const isSameKeysLength = Object.keys(obj1).length === Object.keys(obj2).length;
+//     if (!isSameKeysLength) return false;
+//     for (const key in obj1) {
+//         if (void 0 === obj2[key]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
-function isAllSameItemArray(nodes: unknown[]) {
-  if (nodes.length < 2) return false;
-  let lastType = '';
-  let lastNode;
-  for (const node of nodes) {
-    let currentType: string = typeof node;
-    // 非首次
-    // console.log({ lastType, currentType });
-    if ('' !== lastType) {
-      // 类型相同
-      if (lastType == currentType) {
-        if (isArray(node)) {
-          currentType = 'array';
-          if (!isAllSameItemArray(node)) {
-            return false;
-          }
-        } else if (isObject(node)) {
-          currentType = 'object';
-          const isSameKeys = hasSameKeysObject(node, lastNode as KV);
-          console.log({ isSameKeys });
-          if (!isSameKeys) {
-            return false;
-          }
+export function isSameScheme(input1: unknown, input2: unknown) {
+    const type = [getType(input1), getType(input2)];
+    // 类型相同,
+    // 继续判断引用类型的值
+    if (type[0] === type[1]) {
+        if ('object' === type[0]) {
+            const obj1 = input1 as KV;
+            const obj2 = input2 as KV;
+            const isSameKeysLength = Object.keys(obj1).length === Object.keys(obj2).length;
+
+            // 键值数量不同
+            if (!isSameKeysLength) return false;
+
+            // 键值数量相同
+            // 判断值的类型是否相同
+            for (const key in obj1) {
+                // 有不同的键值
+                if (void 0 === obj2[key]) {
+                    return false;
+                }
+                // 判断值的类型是否相同
+                else if (!isSameScheme(obj1[key], obj2[key])) {
+                    return false;
+                }
+            }
+            return true;
+        } else if ('array' === type[0]) {
+            const length1 = type[0].length;
+            const length2 = type[1].length;
+            if (length1 !== length2) {
+                return false;
+            } else {
+                const length = Math.max(length1, length2);
+                for (let i = 0; i < length; i++) {
+                    const isSame = isSameScheme((input1 as unknown[])[i], (input2 as unknown[])[i]);
+                    if (!isSame) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        } else {
+            return true;
         }
-      } else {
+    } else {
         return false;
-      }
     }
-
-    lastType = currentType;
-    lastNode = node;
-  }
-  return true;
 }
 
-function isSameSchemeArray(arr1:unknown[],arr2:unknown[]):boolean{
-  if(arr2.length !== arr1.length) return false;
-  
-}
-
-function hasSameKeysObject(obj1: KV, obj2: KV):boolean {
-  const isSameKeysLength = Object.keys(obj1).length === Object.keys(obj2).length;
-  if (!isSameKeysLength) return false;
-  for (const key in obj1) {
-    if (void 0 === obj2[key]) {
-      return false;
+function getType(input: unknown): string {
+    const type = typeof input;
+    if ('object' === type) {
+        if (isArray(input)) {
+            return 'array';
+        } else if (null === input) {
+            return 'null';
+        } else {
+            return 'object';
+        }
+    } else {
+        return type;
     }
-  }
-  return true;
 }
