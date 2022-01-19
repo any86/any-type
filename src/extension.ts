3d1a68b5b80b54f88e9f3650c7722be707a1cd50
Russell <383514580@qq.com>
2022-01-19 16:02:44 +0800

    feat: 生成后自动插入到编辑器中

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,41 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { window, commands, env, Range, type ExtensionContext } from 'vscode';
 import getType from './getType';
 
-export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('extension.genTsType', async () => {
+export function activate(context: ExtensionContext) {
+	context.subscriptions.push(commands.registerCommand('extension.genTsType', async () => {
 		try {
-			const input = await vscode.window.showInputBox({ title: '⚡请输入JSON数据' });
+			const input = await window.showInputBox({ title: '⚡请输入JSON数据' });
 			if (input) {
 				const output = getType(input)
-				await vscode.env.clipboard.writeText(output);
-				vscode.window.showInformationMessage('类型生成完毕, 已复制到剪贴板')
+				insertText(output);
+				await env.clipboard.writeText(output);
+				window.showInformationMessage('类型生成成功, 已复制到剪贴板.')
 			}
 		} catch (error) {
-			vscode.window.showErrorMessage(error as  string);
+			window.showErrorMessage(error as string);
 		}
 	}));
+}
+
+
+/**
+ * 插入文字到编辑器
+ * @param text 文字
+ */
+function insertText(text: string) {
+	const editor = window.activeTextEditor;
+	if (editor) {
+		const { selections } = editor;
+		editor.edit(editBuilder => {
+			selections.forEach(selection => {
+				const { start, end } = selection;
+				const range = new Range(start, end);
+				editBuilder.replace(range, text);
+			});
+		});
+	}
 }
 
 export function deactivate() { }
